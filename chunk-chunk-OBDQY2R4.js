1fd9898580173c03ad8d651dcc600e0738d1e3d5
Kevin De Jesus <38739351+gizaom@users.noreply.github.com>
2026-08-30 19:13:55 -0400

    chore: remove public Field OS demo

--- chunk-chunk-OBDQY2R4.js
+++
@@ -1,5 +0,0 @@
-import{render as a}from"https://esm.sh/preact@10.26.9";
-import{useState as b,useEffect as c,useRef as d}from"https://esm.sh/preact@10.26.9/hooks";
-import{Suspense as e,lazy as f}from"https://esm.sh/preact@10.26.9/compat";
-import{jsx as g}from"https://esm.sh/preact@10.26.9/jsx-runtime";
-export{a,b,c,d,e,f,g};
